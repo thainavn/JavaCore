996777533fb22b1810e2ffda5cc4390144bca1a0
Thai Ngo Anh <thaina@vmogroup.com>
2024-01-04 23:10:40 +0700

    string

---
+++ java new/src/reversedemo.java
@@ -0,0 +1,12 @@
+
+public class reversedemo {
+
+    public static void main(String[] args) {
+        String s = "Rahul";
+        for (int i = 4; i >=0; i--) {
+            s.charAt(i);
+        }
+    }
+
+
+}
--- java new/src/stringclassdemo.java
+++ java new/src/stringclassdemo.java
@@ -1,15 +1,25 @@
 
 public class stringclassdemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// String: it is one of pre-built class in Java
-		// 1. String literal
-		// 2. By creating object of string
-		String a = "hello";// string literal
-		String b = "hello";
-		String ab = new String("hello");// with string class
-	}	String b = new String("hello");
-	
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // String: it is one of pre-built class in Java
+        // 1. String literal
+        // 2. By creating object of string
+        String a = " java/training";// string literal
+        System.out.println(a.charAt(2));
+        System.out.println(a.indexOf("1"));
+        System.out.println(a.substring(3, 6));
+        System.out.println(a.substring(5));
+        System.out.println(a.concat(" rahul teachers"));
+        System.out.println(a.trim());
+        System.out.println(a.toUpperCase());
+        System.out.println(a.toLowerCase());
+        String arr[] = a.split("/");
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(a.replace("a", "e"));
+    }
+
 
 }
