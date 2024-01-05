90fa4ae67f252f5aa749913fd381bf3a679eea64
thaina <thaina@vmogroup.com>
2024-01-05 08:46:55 +0700

    Update reversedemo.java

--- java new/src/reversedemo.java
+++ java new/src/reversedemo.java
@@ -4,7 +4,7 @@ public class reversedemo {
     public static void main(String[] args) {
         String s = "Rahul";
         for (int i = 4; i >=0; i--) {
-            s.charAt(i);
+            s.charAt(i);a
         }
     }
 
