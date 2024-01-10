e073a73f73058caa763184a837eb558de493ccd7
Thai Ngo Anh <thaina@vmogroup.com>
2024-01-10 11:32:36 +0700

    something

---
+++ java new/src/StaticVar.java
@@ -0,0 +1,13 @@
+public class StaticVar {
+    String name;
+    String address;
+    String city;
+
+    StaticVar(String name, String Street, String city) {
+
+    }
+
+    public static void main(String[] args) {
+        StaticVar obj = new StaticVar("Bob","Thallium","Bangalore");
+    }
+}
