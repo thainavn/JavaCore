7b02a8909ef5b28261191bf9813e19fdfa65963b
thaina <thaina@vmogroup.com>
2024-01-11 17:09:32 +0700

    jesus

--- java new/src/arrayListExample.java
+++ java new/src/arrayListExample.java
@@ -6,14 +6,21 @@ public class arrayListExample {
         ArrayList<String> a = new ArrayList<String>();
         a.add("Thai");
         a.add("Bob");
+        a.add("Bob");
         System.out.println(a);
         a.add(0, "student");
+        a.add(4, "student");
         System.out.println(a);
-        a.remove(1);
-        System.out.println(a);
-        a.remove("Bob");
-        System.out.println(a);
-        a.removeAll(a);
-        System.out.println(a);
+//        a.remove(1);
+//        System.out.println(a);
+//        a.remove("Bob");
+//        System.out.println(a);
+//        a.removeAll(a);
+//        System.out.println(a);
+        System.out.println(a.get(2));
+        System.out.println(a.contains("Thai"));
+        System.out.println(a.indexOf("Bob"));
+        System.out.println(a.isEmpty());
+        System.out.println(a.size());
     }
 }
---
+++ java new/src/hashSetExample.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class hashSetExample {
+    public static void main(String[] args) {
+        HashSet<String> hs = new HashSet<String>();
+        hs.add("USA");
+        hs.add("UK");
+        hs.add("India");
+        hs.add("he");
+        hs.add("she");
+        hs.add("India");
+        System.out.println(hs);
+//        System.out.println(hs.remove("UK"));
+        System.out.println(hs.isEmpty());
+        System.out.println(hs.size());
+        Iterator<String> i = hs.iterator();
+        while (i.hasNext()){
+            System.out.println(i.next());
+        }
+//        System.out.println(i.next());
+//        System.out.println(i.next());
+    }
+}
