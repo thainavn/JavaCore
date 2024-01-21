303f0c6cb759b4212c470f4d66781cdbea3ef228
Thai Ngo Anh <thaina@vmogroup.com>
2024-01-21 11:28:35 +0700

    df

--- java new/src/ForWhileLoop/WhileDemo.java
+++ java new/src/ForWhileLoop/WhileDemo.java
@@ -4,7 +4,7 @@ public class WhileDemo {
 
     public static void main(String[] args) {
         int i = 10;
-        while (i > 10) {
+        while (i < 10) {
             System.out.println(i);
             i--;
         }
